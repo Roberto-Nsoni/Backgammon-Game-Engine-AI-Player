86fd282b60ef4913c4e5171bcf46b00987430fea
Roberto-Nsoni <roberto.lulenga.nsoni@estudiantat.upc.edu>
2025-03-10 17:51:50 +0100

    Classe Board V0.1.0
    Implementació bàsica que permet accedir als atributs de la classe Board

--- backgammon/board.py
+++ backgammon/board.py
@@ -25,7 +25,6 @@ class Dice:
         return self.die1 == self.die2
 
     def is_valid(self) -> bool:
-
         return 1 <= self.die1 <= 6 and 1 <= self.die2 <= 6
 
 
@@ -62,53 +61,84 @@ class Move:
 
 class Board:
 
+    # Parameters:
+    
+    _dice: Dice # Daus que han sortit per la jugada actual
+    _barW: int # Nombre de fitxes blanques a la barra
+    _barB: int # Nombre de fitxes negres a la barra
+    _turn: int # Torn actual de la partida
+    _cells: list[int] # Nombre de fitxes, per posició, al tauler
+    _offW: int # Nombre de fitxes que han salvat les blanques
+    _offB: int # Nombre de fitxes que han salvat les negres
+
     def __init__(self, dice: Dice, turn: int = 1, cells: list[int] | None = None, barW: int = 0, barB: int = 0) -> None:
         """..."""
-        ...
+        self._dice = dice.copy()
+        self._barW = barW
+        self._barB = barB
+        self._turn = turn
+        self._offW = 0
+        self._offB = 0
 
+        # Generació de les caselles
+        if cells:
+            self._cells = cells
+        else:
+            self._cells = [2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2] # Tauler amb les posicions inicials
+        
     def copy(self) -> Board:
-        """..."""
-        ...
+        """Retorna una copia del tauler actual."""
+        return Board(self.dice(), self.turn(), self.cells(), self.bar(WHITE), self.bar(BLACK))
 
     def flip(self) -> Board:
-        """..."""
-        ...
+        """Retorna el mateix tauler amb els colors i sentits invertits."""
+        return Board(self.dice(), self.turn(), [-i for i in self.cells()], self.bar(BLACK), self.bar(WHITE))
 
     def cells(self) -> list[int]:
-        """..."""
-        ...
+        """Retorna una llista amb el nombre de fitxes, per posicions, al tauler"""
+        return self._cells.copy()
 
     def cell(self, i: int) -> int:
-        """..."""
-        ...
+        """Retorna el nombre de fitxes que hi ha a una posició concreta del tauler.
+        Prec: 0 <= i <= 23"""
+        return self._cells[i]
 
     def bar(self, player: Player) -> int:
-        """..."""
-        ...
+        """Retorna el nombre de fitxes que té a la barra el jugador (blanc o negre)"""
+        return self._barW if player == WHITE else self._barB
 
     def off(self, player: Player) -> int:
-        """..."""
-        ...
+        """Retorna el nombre de fitxes que té salvades el jugador (blanc o negre)"""
+        return self._offW if player == WHITE else self._offB
 
     def dice(self) -> Dice:
-        """..."""
-        ...
+        """Retorna una copia del dau actual que s'està jugant"""
+        return self._dice
 
     def turn(self) -> int:
-        """..."""
-        ...
+        """Retorna el torn actual que s'està jugant"""
+        return self._turn
 
     def current(self) -> OptionalPlayer:
-        """..."""
-        ...
+        """Retorna el jugador que li toca moure al torn actual"""
+        if self._turn % 2 == 1:
+            return WHITE
+        else:
+            return BLACK
 
     def winner(self) -> OptionalPlayer:
-        """..."""
-        ...
+        """Retorna el jugador que ha guanyat la partida (si encara no ha acabat, retorna None)"""
+
+        if self.off(WHITE) == 15:
+            return WHITE
+        elif self.off(BLACK) == 15:
+            return BLACK    
+        else:
+            return None
 
     def over(self) -> bool:
-        """..."""
-        ...
+        """Retorna "True" si la partida ha acabat, retorna "False" alternament."""
+        return not (self.winner() == None)
 
     def valid_moves(self) -> list[Move]:
         """..."""
@@ -121,7 +151,7 @@ class Board:
     def play(self, move: Move) -> Board:
         """..."""
         ...
-
+        
     def next(self, dice: Dice) -> Board:
         """..."""
         ...
